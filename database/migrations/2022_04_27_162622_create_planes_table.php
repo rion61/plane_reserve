cdea2ccb17a0b5e79dd59d73a5975534a313a07b
yuk0624 <mapiharuioxmasayuka@gmail.com>
2022-04-28 09:57:15 +0900

    first commit

---
+++ database/migrations/2022_04_27_162622_create_planes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('departure');
+            $table->string('arrival');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planes');
+    }
+}
---
+++ database/migrations/2022_04_27_162702_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedbigInteger('user_id');
+            $table->unsignedbigInteger('plane_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('plane_id')->references('id')->on('planes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
