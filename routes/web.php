f4e72c6912c63e2e31f2709015aa91eff13ec60b
tk92mevius <taichi.k.0902@gmail.com>
2022-05-15 15:39:24 +0900

    飛行機一覧、作成、確認、会員一覧画面、飛行機予約画面

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/views', [App\Http\Controllers\SystemController::class, 'managerhome'])->name('managershome');
+Route::get('/airplane_reserve', [App\Http\Controllers\SystemController::class, 'airplane_reserve'])->name('airplane_reserve');
+Route::get('/airplane_create', [App\Http\Controllers\SystemController::class, 'airplane_create'])->name('airplane_create');
+Route::get('/create_check', [App\Http\Controllers\SystemController::class, 'create_check'])->name('create_check');
+Route::get('/airplane_edit', [App\Http\Controllers\SystemController::class, 'airplane_edit'])->name('airplane_edit');
+Route::get('/manager_profile_edit', [App\Http\Controllers\SystemController::class, 'manager_profile_edit'])->name('manager_profile_edit');
+Route::get('/airplane_list', [App\Http\Controllers\SystemController::class, 'airplane_list'])->name('airplane_list');
+Route::get('/reserve_list', [App\Http\Controllers\SystemController::class, 'reserve_list'])->name('reserve_list');
+Route::get('/member_list', [App\Http\Controllers\SystemController::class, 'member_list'])->name('member_list');
