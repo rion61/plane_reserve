1b72d9e532e2af30d12028b5a0cd1f47b0b5411b
yuk0624 <mapiharuioxmasayuka@gmail.com>
2022-05-18 11:59:37 +0900

    トップ画面、予約確認画面、予約確定処理、予約一覧画面、予約削除確認画面、予約削除処理

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\MypageController;
+use App\Http\Controllers\ReserveController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +26,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//TOPページの表示
+Route::get('/top',[ReserveController::class,'index'])->name('top');
+
+//選択した飛行機情報を持って予約確認ページへ
+Route::get('/showdetail/{id}',[ReserveController::class,'show'])->name('detail'
+);
+
+// ログイン中のユーザーが予約を確定する
+Route::post('/completed/{id}', [ReserveController::class,'store'])->name('completed');
+
+// ログインユーザーの予約情報確認画面の表示
+Route::get('/mybooks',[BookController::class,'index'])->name('books');
+
+// 予約削除確認画面
+Route::get('/mybook/{id}',[BookController::class,'show'])->name('book');
+
+// 予約の削除
+Route::post('/mybook{id}',[BookController::class,'update'])->name('delete');
