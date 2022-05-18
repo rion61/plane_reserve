b10413b163fad1aaace0e3caff00c314fe12d613
fjkk <fujikake.takayoshi@gmail.com>
2022-05-18 14:43:47 +0900

    backend_user merge to develop temporary

--- app/Models/plane.php
+++ app/Models/plane.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class plane extends Model
+class Plane extends Model
 {
     use HasFactory;
 }
